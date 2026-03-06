14b93783809405db997ba9bb08da57cc596a8199
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-03-06 19:25:35 +0530

    Update variables.tf

--- infrastructure/modules/aks/variables.tf
+++ infrastructure/modules/aks/variables.tf
@@ -1,39 +1,14 @@
-variable "cluster_name" {
-  description = "AKS cluster name"
-  type        = string
-}
-
-variable "rg_name" {
-  description = "Resource group name"
-  type        = string
-}
-
-variable "location" {
-  description = "Azure region"
-  type        = string
-}
-
-variable "vnet_subnet_id" {
-  description = "Subnet ID for AKS nodes"
-  type        = string
-}
-
 variable "kubernetes_version" {
   description = "AKS Kubernetes version"
   type        = string
 }
 
 variable "node_count" {
-  description = "Initial node count"
+  description = "AKS node count"
   type        = number
 }
 
 variable "vm_size" {
-  description = "VM size for AKS nodes"
+  description = "AKS node VM size"
   type        = string
-}
-
-variable "tags" {
-  description = "Common tags"
-  type        = map(string)
 }
