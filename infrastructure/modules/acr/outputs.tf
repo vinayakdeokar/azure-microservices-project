11b4c67bbadc54b65758021d840e9670f2da50fd
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-03-06 17:47:46 +0530

    Update outputs.tf

--- infrastructure/modules/acr/outputs.tf
+++ infrastructure/modules/acr/outputs.tf
@@ -1,3 +1,9 @@
+output "acr_id" {
+  description = "ID of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.id
+}
+
 output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
+  description = "Login server of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.login_server
 }
