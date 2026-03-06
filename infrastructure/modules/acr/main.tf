767d434dd4a7b6cecb64ddc8dbd7508788c65c1c
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-03-06 17:47:30 +0530

    Update main.tf

--- infrastructure/modules/acr/main.tf
+++ infrastructure/modules/acr/main.tf
@@ -2,12 +2,9 @@ resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = var.rg_name
   location            = var.location
-  sku                 = "Basic"
-  admin_enabled       = true
+
+  sku           = "Basic"
+  admin_enabled = true
 
   tags = var.tags
-}
-
-output "acr_id" {
-  value = azurerm_container_registry.acr.id
 }
