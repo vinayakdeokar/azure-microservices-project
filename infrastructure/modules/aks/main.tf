8ba2977fd8ea93463c74f74b75577eee9471554c
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-03-06 19:25:20 +0530

    Update main.tf

--- infrastructure/modules/aks/main.tf
+++ infrastructure/modules/aks/main.tf
@@ -1,30 +1,16 @@
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.cluster_name
-  location            = var.location
-  resource_group_name = var.rg_name
-  dns_prefix          = var.cluster_name
+# -------------------------------
+# Azure Kubernetes Service
+# -------------------------------
+module "aks" {
+  source         = "./modules/aks"
+  rg_name        = module.resource_group.rg_name
+  location       = var.location
+  cluster_name   = var.aks_name
+  vnet_subnet_id = module.network.subnet_id
 
   kubernetes_version = var.kubernetes_version
-
-  default_node_pool {
-    name           = "system"
-    node_count     = var.node_count
-    vm_size        = var.vm_size
-    vnet_subnet_id = var.vnet_subnet_id
-
-    enable_auto_scaling = true
-    min_count           = 1
-    max_count           = 3
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  network_profile {
-    network_plugin = "azure"
-    load_balancer_sku = "standard"
-  }
+  node_count         = var.node_count
+  vm_size            = var.vm_size
 
   tags = var.tags
 }
