398e0b454da42610bc21fba85ff6567a4189c3a2
root <root@DESKTOP-K4UBP7Q.localdomain>
2026-03-06 18:54:41 +0530

    fix: terraform formatting

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,11 +1,11 @@
 output "resource_group_name" {
-  value = module.resource_group.rg_name  
+  value = module.resource_group.rg_name
 }
 
 output "aks_cluster_name" {
-  value = module.aks.aks_name 
+  value = module.aks.aks_name
 }
 
 output "acr_login_server" {
-  value = module.acr.acr_login_server 
+  value = module.acr.acr_login_server
 }
