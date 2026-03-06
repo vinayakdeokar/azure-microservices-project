5b5236bd135dc6814ae45be8fcf77507052850a6
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-03-06 17:50:21 +0530

    Update outputs.tf

--- infrastructure/modules/aks/outputs.tf
+++ infrastructure/modules/aks/outputs.tf
@@ -1,3 +1,15 @@
+output "aks_id" {
+  description = "AKS cluster ID"
+  value       = azurerm_kubernetes_cluster.aks.id
+}
+
 output "aks_name" {
-  value = azurerm_kubernetes_cluster.aks.name
+  description = "AKS cluster name"
+  value       = azurerm_kubernetes_cluster.aks.name
+}
+
+output "kube_config" {
+  description = "Kubeconfig for cluster access"
+  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive   = true
 }
