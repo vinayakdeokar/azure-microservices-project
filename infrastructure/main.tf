4288e559125e4d68feff489a95e1dbf4cc5df5bc
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-03-06 14:32:31 +0530

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,9 +7,10 @@ module "resource_group" {
   source   = "./modules/resource_group"
   rg_name  = var.resource_group_name
   location = var.location
+  tags     = var.tags
 }
 
-# 2. Networking Module (Output: subnet_id)
+# 2. Networking Module
 module "network" {
   source                = "./modules/vnet"
   rg_name               = module.resource_group.rg_name
@@ -17,6 +18,7 @@ module "network" {
   prefix                = var.prefix
   vnet_address_space    = var.vnet_space
   subnet_address_prefix = var.subnet_prefix
+  tags                  = var.tags
 }
 
 # 3. ACR Module
@@ -25,13 +27,15 @@ module "acr" {
   rg_name  = module.resource_group.rg_name
   location = var.location
   acr_name = var.acr_name
+  tags     = var.tags
 }
 
-# 4. AKS Module (Input: subnet_id from network module)
+# 4. AKS Module
 module "aks" {
   source         = "./modules/aks"
   rg_name        = module.resource_group.rg_name
   location       = var.location
   cluster_name   = var.aks_name
   vnet_subnet_id = module.network.subnet_id
+  tags           = var.tags
 }
