ae9185ae9e29d3587b50453387f847d27f555209
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-03-06 12:46:53 +0530

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -31,7 +31,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-# 4. Role Assignment (AKS la ACR madhun image pull karaychi permission dene)
+# 4. Role Assignment 
 resource "azurerm_role_assignment" "aks_to_acr" {
   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
