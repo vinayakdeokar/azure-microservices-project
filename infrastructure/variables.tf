bedacfa105d7e663e10c69a07701c649e934383f
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-03-06 14:34:46 +0530

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -5,3 +5,12 @@ variable "aks_name" { type = string }
 variable "prefix" { type = string }
 variable "vnet_space" { type = list(string) }
 variable "subnet_prefix" { type = list(string) }
+variable "tags" {
+  type        = map(string)
+
+  default = {
+    Environment = "Dev"
+    Project     = "E-Commerce-Microservice"
+    Owner       = "ECM"
+  }
+}
