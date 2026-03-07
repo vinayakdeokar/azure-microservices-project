d8e419312cb39f44b6f5849950c9cdeeb81dfe63
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-03-08 00:37:00 +0530

    Update index.js

--- src/order-api/index.js
+++ src/order-api/index.js
@@ -5,12 +5,21 @@ const app = express();
 app.use(express.json());
 
 const client = new MongoClient("mongodb://mongodb-service:27017");
-const db = client.db("ecommerce");
 
-app.post("/order", async (req, res) => {
-  const order = req.body;
-  await db.collection("orders").insertOne(order);
-  res.send({ status: "Order placed" });
-});
+async function start() {
 
-app.listen(3002, () => console.log("Order API running"));
+  await client.connect()
+
+  const db = client.db("ecommerce")
+
+  app.post("/order", async (req, res) => {
+    const order = req.body
+    await db.collection("orders").insertOne(order)
+    res.send({status:"Order placed"})
+  })
+
+  app.listen(3002, () => console.log("Order API running"))
+
+}
+
+start()
