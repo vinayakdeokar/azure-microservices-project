0d838505c844cb85251a67bc6c9f15fdc8360ed4
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-03-07 20:02:52 +0530

    Create main.tf

---
+++ infrastructure/modules/ingress/main.tf
@@ -0,0 +1,13 @@
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+
+  namespace        = "ingress-nginx"
+  create_namespace = true
+
+  set {
+    name  = "controller.service.type"
+    value = "LoadBalancer"
+  }
+}
