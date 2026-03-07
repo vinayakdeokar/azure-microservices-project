93f82e18a750953b14275dcf3b0367d4e64e4aa7
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-03-07 20:20:04 +0530

    Update provider.tf

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -13,6 +13,14 @@ terraform {
     key                  = "terraform.tfstate"
   }
 }
+provider "helm" {
+  kubernetes {
+    host                   = module.aks.host
+    client_certificate     = base64decode(module.aks.client_certificate)
+    client_key             = base64decode(module.aks.client_key)
+    cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
+  }
+}
 
 provider "azurerm" {
   features {}
