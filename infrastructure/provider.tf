66bb395d647ef46508006ccbf9bc3971312c1a43
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-03-07 21:00:44 +0530

    Update provider.tf

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -4,6 +4,16 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
   }
 
   backend "azurerm" {
@@ -19,3 +29,13 @@ provider "azurerm" {
 }
 
 data "azurerm_client_config" "current" {}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
