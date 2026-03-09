3a45df8dfef10a9a32a891cbada08f09fa146a58
root <root@DESKTOP-K4UBP7Q.localdomain>
2026-03-09 12:26:28 +0530

    final code

--- src/product-api/app.py
+++ src/product-api/app.py
@@ -1,15 +1,22 @@
 from flask import Flask, jsonify
 from pymongo import MongoClient
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) # He browser sathi khup garjeche ahe
 
+# Cluster madhe 'mongodb-service' vapra
 client = MongoClient("mongodb://mongodb-service:27017/")
 db = client["ecommerce"]
 
-@app.route("/products")
-def products():
-    products = list(db.products.find({}, {"_id":0}))
-    return jsonify(products)
+@app.route("/products") # Path /products theva
+def get_products():
+    try:
+        # DB madhun data fetech kara
+        products = list(db.products.find({}, {"_id": 0}))
+        return jsonify(products)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3001)
+    app.run(host="0.0.0.0", port=3001) # Port 3001
