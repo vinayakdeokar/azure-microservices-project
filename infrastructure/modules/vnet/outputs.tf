ced7bd2364c309ea931d537599f9f7683f078766
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-03-09 16:50:00 +0530

    a

--- infrastructure/modules/vnet/outputs.tf
+++ infrastructure/modules/vnet/outputs.tf
@@ -4,13 +4,13 @@ output "vnet_id" {
 }
 
 output "app_subnet_id" {
-  value = azurerm_subnet.subnets["app-subnet"].id
+  value = azurerm_subnet.subnets["dev-app-subnet-1"].id
 }
 
 output "dmz_subnet_id" {
-  value = azurerm_subnet.subnets["dmz-subnet"].id
+  value = azurerm_subnet.subnets["dev-dmz-subnet-1"].id
 }
 
 output "data_subnet_id" {
-  value = azurerm_subnet.subnets["data-subnet"].id
+  value = azurerm_subnet.subnets["dev-data-subnet-1"].id
 }
