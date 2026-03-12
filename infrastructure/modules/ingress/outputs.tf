75cdd4489512456c8fe85a0e5debd05a95e59093
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-03-12 23:36:06 +0530

    update app gatway

--- infrastructure/modules/ingress/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ingress_name" {
-  value = helm_release.nginx_ingress.name
-}
--- infrastructure/modules/ingress/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "tags" {
-  description = "Common tags"
-  type        = map(string)
-}
-variable "subnet_id" {
-  type = string
-}
