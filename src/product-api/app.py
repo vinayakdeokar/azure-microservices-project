c8fab9ac86b8583b7e0841ad914ad31322403463
Vinayak Deokar <vinayakdeokar121@gmail.com>
2026-03-13 11:56:56 +0530

    update index.hyml

--- src/product-api/app.py
+++ src/product-api/app.py
@@ -3,16 +3,16 @@ from pymongo import MongoClient
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) # He browser sathi khup garjeche ahe
+CORS(app) 
 
-# Cluster madhe 'mongodb-service' vapra
+
 client = MongoClient("mongodb://mongodb-service:27017/")
 db = client["ecommerce"]
 
-@app.route("/products") # Path /products theva
+@app.route("/products") 
 def get_products():
     try:
-        # DB madhun data fetech kara
+        
         products = list(db.products.find({}, {"_id": 0}))
         return jsonify(products)
     except Exception as e:
